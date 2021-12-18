c4757c56e8e9184ba1654e68f5991f2a1fe4ca48
Gabriolo <78807964+Gabriolo@users.noreply.github.com>
2021-12-18 19:11:46 +0100

    Aggiungere i file di progetto.

--- ScreenCapture/main.cpp
+++ ScreenCapture/main.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
 #include "ScreenRecorder.h"
+#include "AudioRecorder.h"
 
 using namespace std;
 
 /* driver function to run the application */
 int main()
-{
+{	/*
 	ScreenRecorder screen_record;
 
 	screen_record.openCamera();
@@ -17,4 +18,30 @@ int main()
 	cout << "\nprogram executed successfully\n";
 
 	return 0;
+	*/
+    puts("==== Audio Recorder ====");
+    avdevice_register_all();
+
+    AudioRecorder recorder{ "../media/testAudio.aac","" };
+    try {
+        recorder.Open();
+        recorder.Start();
+
+        //record 10 seconds.
+        std::this_thread::sleep_for(10s);
+
+        recorder.Stop();
+        string reason = recorder.GetLastError();
+        if (!reason.empty()) {
+            throw std::runtime_error(reason);
+        }
+    }
+    catch (std::exception& e) {
+        fprintf(stderr, "[ERROR] %s\n", e.what());
+        exit(-1);
+    }
+
+    puts("END");
+    return 0;
+
 }
