5b383e010740121e9a30a1c60fb60b183f828782
v0q1n8 <isa.lom@hotmail.it>
2022-01-18 21:41:20 +0100

    Esempio di codice per ottenere dinamicamente le dimensioni correnti del display

--- ScreenCapture/test_main.cpp
+++ ScreenCapture/test_main.cpp
@@ -1,14 +1,38 @@
 /*  TODO: File da eliminare prima della consegna */
 #include <bits/stdc++.h>
 #include "ScreenRecorder.h"
+#include <X11/Xlib.h>
+#include<tuple>
+
 
 using namespace std;
 
 /* driver function to run the application */
+
+
+
+tuple<int, int> retrieveDisplayDimention()
+{
+	Display* disp = XOpenDisplay(NULL);
+	Screen*  scrn = DefaultScreenOfDisplay(disp);
+	int height = scrn->height;
+	int width  = scrn->width;
+	//cout << "\nStampa dimensioni display\n";
+	//cout << width<<"x"<< height;
+	
+	return make_tuple(height, width);
+	
+}
+
+
 int main()
 {
-
-	cout << "\nOK\n";
-
+	int height, width;
+	tie(height, width)=retrieveDisplayDimention();
+	cout << "\nStampa dimensioni display\n";
+	cout << width<<"x"<< height;
 	return 0;
 }
+
+
+
