286e71c0aefc7fcf7b1e18ce94e90b118731cea3
lordef <lorenzo.defilippis02@gmail.com>
2022-01-18 21:15:30 +0100

    added file to test codes

---
+++ ScreenCapture/test_main.cpp
@@ -0,0 +1,14 @@
+/*  TODO: File da eliminare prima della consegna */
+#include <bits/stdc++.h>
+#include "ScreenRecorder.h"
+
+using namespace std;
+
+/* driver function to run the application */
+int main()
+{
+
+	cout << "\nOK\n";
+
+	return 0;
+}
