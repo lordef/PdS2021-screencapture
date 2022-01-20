52bc62864c3180d45b2e9e1e64e91078c3fb5c7c
Gabriolo <78807964+Gabriolo@users.noreply.github.com>
2022-01-20 20:26:29 +0100

    Memory leakage risolto

--- ScreenCapture/main.cpp
+++ ScreenCapture/main.cpp
@@ -1,12 +1,11 @@
 #include <bits/stdc++.h>
 #include "ScreenRecorder.h"
-#include "AudioRecorder.h"
 
 using namespace std;
 
 /* driver function to run the application */
 int main()
-{	/*
+{
 	ScreenRecorder screen_record;
 
 	screen_record.openCamera();
@@ -15,33 +14,25 @@ int main()
 	//screen_record.start();
 	//screen_record.stop();
 
+
+	/* Metodi per distinguere il SO */
+	/* Metodo 1 */
+	// #ifdef __linux__
+    // cout << "esiste MACRO __linux__" << endl;
+    // #endif
+    // #ifdef _WIN32
+    // cout << "esiste MACRO _WIN32" << endl;
+    // #endif
+
+	/* Metodo 2 */
+    // #ifdef __linux__
+    // cout << "esiste MACRO __linux__" << endl;
+    // #elif defined(_WIN32)
+    // cout << "esiste MACRO _WIN32" << endl;
+    // #endif
+	/*****/
+
 	cout << "\nprogram executed successfully\n";
 
 	return 0;
-	*/
-    puts("==== Audio Recorder ====");
-    avdevice_register_all();
-
-    AudioRecorder recorder{ "../media/testAudio.aac","" };
-    try {
-        recorder.Open();
-        recorder.Start();
-
-        //record 10 seconds.
-        std::this_thread::sleep_for(10s);
-
-        recorder.Stop();
-        string reason = recorder.GetLastError();
-        if (!reason.empty()) {
-            throw std::runtime_error(reason);
-        }
-    }
-    catch (std::exception& e) {
-        fprintf(stderr, "[ERROR] %s\n", e.what());
-        exit(-1);
-    }
-
-    puts("END");
-    return 0;
-
 }
