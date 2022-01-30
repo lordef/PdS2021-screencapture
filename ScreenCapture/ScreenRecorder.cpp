3c1d803ac5f7b37cb5f6a3d57d9fbc146307b018
lordef <lorenzo.defilippis02@gmail.com>
2022-01-30 13:33:18 +0100

    UPDATED: new version or timestamp according to POSIX standard

--- ScreenCapture/ScreenRecorder.cpp
+++ ScreenCapture/ScreenRecorder.cpp
@@ -32,24 +32,38 @@ tuple<int, int> retrieveDisplayDimention()
 std::string retrieveTimestamp()
 {
 
-    std::string current_time;   
+    std::string current_time;
 
-    #ifdef __linux__ 
-    //#TODO: dovrebbe tranquillamente funzionare anche per Windows
+    #ifdef __linux__
+        //#TODO: dovrebbe tranquillamente funzionare anche per Windows
         // declaring argument of time()
-        time_t my_time = time(nullptr);
-        //ctime() used to give the present time
-        current_time = ctime(&my_time);   
-        current_time.erase(current_time.end()-1, current_time.end());
-        std::replace(current_time.begin(), current_time.end(), ' ', '_');
+        const time_t now = time(nullptr);
+
+        /* ctime() used to give the present time */
+        // current_time = ctime(&my_time); //ctime obsoleta per lo standard POSIX
+
+        /* Main source of this solution: https://en.cppreference.com/w/cpp/chrono/c/asctime */
+        char buf[64];
+        if (strftime(buf, sizeof buf, "%c\n", std::localtime(&now)) || strftime(buf, sizeof buf, "%a %b %e %H:%M:%S %Y\n", std::localtime(&now)))
+        {
+            // std::cout << std::setw(40) << "    strftime %c" << buf;
+            // std::cout << std::setw(40) << "    strftime %a %b %e %H:%M:%S %Y" << buf;
+            current_time = buf;
+            current_time.erase(current_time.end() - 1, current_time.end());
+            std::replace(current_time.begin(), current_time.end(), ' ', '_');
+        }
+        // else {
+        //     std::cerr << "Error in retrieving timestamp" << std::endl;
+        //     exit(-1);
+        // }
     #elif _WIN32
-         time_t result = time(nullptr);
-         stringstream ss;
-         ss << time;
-         current_time = ss.str();
+        time_t result = time(nullptr);
+        stringstream ss;
+        ss << time;
+        current_time = ss.str();
     #endif
 
-	return current_time;
+    return current_time;
 }
 
 /****************************/
